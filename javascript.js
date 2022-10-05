c167111f889d964546879e027acfd2a9bc6dc7de
jjonatasx <111437851+jjonatasx@users.noreply.github.com>
2022-10-05 09:26:45 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,27 @@
+function calcSum() {
+    let num1 = document.getElementsByName("n1")[0].value;
+    let number2 = document.getElementsByName("n2")[0].value;
+    let sum = Number(num1) + Number(number2);
+    document.getElementsByName("somado")[0].value = sum;
+}
+
+function calcSub() {
+    let num1 = document.getElementsByName("n1")[0].value;
+    let number2 = document.getElementsByName("n2")[0].value;
+    let sum = Number(num1) - Number(number2);
+    document.getElementsByName("somado")[0].value = sum;
+}
+
+function calcmult() {
+    let num1 = document.getElementsByName("n1")[0].value;
+    let number2 = document.getElementsByName("n2")[0].value;
+    let sum = Number(num1) * Number(number2);
+    document.getElementsByName("somado")[0].value = sum;
+}
+
+function calcdiv() {
+    let num1 = document.getElementsByName("n1")[0].value;
+    let number2 = document.getElementsByName("n2")[0].value;
+    let sum = Number(num1) / Number(number2);
+    document.getElementsByName("somado")[0].value = sum;
+}
